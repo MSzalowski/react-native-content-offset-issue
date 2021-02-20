ebcf987c3172b676a671f2b68255cb8564cb3376
Michał Szałowski <m.szalowski@neurosys.com>
2021-02-20 19:16:17 +0100

    add animation

--- App.js
+++ App.js
@@ -6,37 +6,51 @@ import {
   View,
   Text,
   Button,
+  Animated,
 } from "react-native";
 
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const App = () => {
+  const translation = useRef(Animated.Value(0));
   const [items, setItems] = useState(20);
+
+  useEffect(() => {
+    Animated.timing(translation.current, {
+      useNativeDriver: true,
+      toValue: 50,
+      duration: 200,
+    });
+  }, [items.length]);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView style={styles.scrollView}>
-        {[...Array(items).keys()].map((item) => (
-          <View
-            key={`item-${item}`}
-            style={{
-              alignItems: "center",
-              height: 50,
-              marginBottom: 16,
-              backgroundColor: `rgb(${item * 9}, ${item * 7}, ${item * 5})`,
-            }}
-          >
-            <Text
+      <Animated.View
+        style={{ transform: [{ translateY: translation.current }] }}
+      >
+        <ScrollView style={styles.scrollView}>
+          {[...Array(items).keys()].map((item) => (
+            <View
+              key={`item-${item}`}
               style={{
-                color: "white",
-                fontSize: 40,
-                fontWeight: "500",
-                lineHeight: 50,
+                alignItems: "center",
+                height: 50,
+                marginBottom: 16,
+                backgroundColor: `rgb(${item * 9}, ${item * 7}, ${item * 5})`,
               }}
-            >{`Item: ${item}`}</Text>
-          </View>
-        ))}
-      </ScrollView>
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 40,
+                  fontWeight: "500",
+                  lineHeight: 50,
+                }}
+              >{`Item: ${item}`}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </Animated.View>
       <View style={[{ backgroundColor: "blue" }, { bottom: 0 }]}>
         <Button
           title="Set items"
